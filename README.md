d8e6462c0940f3c5ce5b3a9b1b026dd1ed74b682
deeliciouscode <schmiderdavid@gmail.com>
2022-04-06 00:15:17 +0200

    baby steps

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # publicflow
 Here is where the Code for my BA will live
+
+## Usefull Commands
+- cargo doc --open : opens documentation of depedencies in browser
+-  
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the no.!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    // println!("The secret number is: {}", secret_number);
+    println!("Please input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number dog!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+    
+    println!("You guessed: {}", guess)
+}
+
+// continue here -> https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
